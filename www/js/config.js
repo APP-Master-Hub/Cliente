14601138f8e195381c265adb209a1bb4e15e8829
Robson Gonçalves <appmasterhub@gmail.com>
2020-06-25 11:25:17 -0300

    Segundo envio!

--- www/js/config.js
+++ www/js/config.js
@@ -2,5 +2,5 @@ var krms_config ={
 	'ApiUrl':"https://piloto.masterhub.com.br/mobileappv2/api",	
 	'AppTitle':"Master Hub®",
 	'ApiKey' : '4m1DDci5rKsyeNlmkwZlSsQDCpg=',	
-	'debug': true
+	'debug': false
 };
